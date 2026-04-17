539c01dfc03379fec62033f71fc773deebfcd3fb
saikat-dev-cse <saikatbehera18@gmail.com>
2026-04-17 11:16:19 +0530

    Update index.php

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div class="center-box">
+    <h2>Login</h2>
+
+    <form action="student.php">
+        <input type="text" placeholder="Email or Reg No" required>
+        <input type="password" placeholder="Password" required>
+        <button class="btn-primary">Login</button>
+    </form>
+
+    <p><a href="signup.php">Create Account</a></p>
+</div>
+
+</body>
+</html>
