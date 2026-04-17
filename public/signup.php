b4ce2333f333ed53a288733103ff82aebbefe9ac
saikat-dev-cse <saikatbehera18@gmail.com>
2026-04-17 11:21:27 +0530

    Add signup.php for user registration form

---
+++ public/signup.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div class="center-box">
+    <h2>Sign Up</h2>
+
+    <form>
+        <input type="text" placeholder="Full Name" required>
+        <input type="text" placeholder="Reg No" required>
+        <input type="email" placeholder="Email" required>
+        <input type="password" placeholder="Password" required>
+
+        <button class="btn-primary">Create Account</button>
+    </form>
+</div>
+
+</body>
+</html>
