86065aea8263b5e4f0925ea37ecbe6494df0acbb
saikat-dev-cse <saikatbehera18@gmail.com>
2026-04-17 16:41:38 +0530

    Delete public/assets directory

--- public/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function show(section) {
-    document.getElementById("dashboard").style.display = "none";
-    document.getElementById("complaints").style.display = "none";
-
-    document.getElementById(section).style.display = "block";
-}
